5c4ec1106c316fb4df55ad45932c5eb52a5f506d
sunoru <s@sunoru.com>
2016-03-11 07:52:16 +0800

    install-julia.sh & simple test

---
+++ install-julia.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install Intel compilers
+wget -q -O /tmp/install-icc.sh \
+    "https://raw.githubusercontent.com/nemequ/icc-travis/master/install-icc.sh"
+/tmp/install-icc.sh --components icc,ifort,mkl 
+source ~/.bashrc
+
+# Get the source of Julia and compile it.
+JULIA_VERSION="release-0.4"
+if [ $# == 1 ]; then
+    JULIA_VERSION=$1
+fi
+echo "Use Julia of version $JULIA_VERSION"
+wget -q -O /tmp/julia.zip "https://github.com/JuliaLang/julia/archive/$JULIA_VERSION.zip"
+unzip -qq /tmp/julia.zip -d /tmp
+mv /tmp/julia-${JULIA_VERSION//\//-} /tmp/julia-source
+cd /tmp/julia-source
+cat "USEICC = 1\nUSEIFC = 1\nUSE_INTEL_MKL = 1\nUSE_INTEL_MKL_FFT = 1\nUSE_INTEL_LIBM = 1\n" > Make.user
+make
+export PATH=$PAHT:/tmp/julia-source$
---
+++ test/simple.jl
@@ -0,0 +1,1 @@
+println("Julia installed successfully!")
