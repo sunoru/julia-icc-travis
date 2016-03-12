b5cb31ec50612c58a12668cc32ef1a94637e3b2d
sunoru <s@sunoru.com>
2016-03-12 10:31:00 +0800

    use root, testing why icc is not used

--- install-julia.sh
+++ install-julia.sh
@@ -3,7 +3,7 @@
 # Install Intel compilers
 wget -q -O /tmp/install-icc.sh \
     "https://raw.githubusercontent.com/sunoru/julia-icc-travis/master/install-icc.sh"
-bash /tmp/install-icc.sh --components icc,ifort,mkl --dest ${HOME}/intel || exit 1
+sudo bash /tmp/install-icc.sh --components icc,ifort,mkl --dest ${HOME}/intel || exit 1
 source ${HOME}/intel/compilers_and_libraries_2016/linux/bin/compilervars.sh intel64 && echo "Source completed"
 
 # Get the source of Julia and compile it.
@@ -18,6 +18,8 @@ mv /tmp/julia-${JULIA_VERSION//\//-} /tmp/julia-source
 cd /tmp/julia-source
 echo "USEICC = 1\nUSEIFC = 1\nUSE_INTEL_MKL = 1\nUSE_INTEL_MKL_FFT = 1\nUSE_INTEL_LIBM = 1\n" > Make.user
 which icc || exit 1
+grep -i "Make.user" Make.inc
+make test
 # make -j 3
 export PATH=$PAHT:/tmp/julia-source/bin
 julia -e 'versioninfo()'
