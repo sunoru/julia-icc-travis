b47401da664dbb4b7dd29a615657a56688b3a807
sunoru <s@sunoru.com>
2016-03-12 10:42:58 +0800

    test if Make.user is included..

--- install-julia.sh
+++ install-julia.sh
@@ -11,15 +11,13 @@ JULIA_VERSION="release-0.4"
 if [ $# == 1 ]; then
     JULIA_VERSION=$1
 fi
-echo "Use Julia of version $JULIA_VERSION"
+echo "Build Julia of version $JULIA_VERSION"
 wget -q -O /tmp/julia.zip "https://github.com/JuliaLang/julia/archive/$JULIA_VERSION.zip"
 unzip -qq /tmp/julia.zip -d /tmp
 mv /tmp/julia-${JULIA_VERSION//\//-} /tmp/julia-source
 cd /tmp/julia-source
-echo "USEICC = 1\nUSEIFC = 1\nUSE_INTEL_MKL = 1\nUSE_INTEL_MKL_FFT = 1\nUSE_INTEL_LIBM = 1\n" > Make.user
+echo "USEICC = 1\nUSEIFC = 1\nUSE_INTEL_MKL = 1\nUSE_INTEL_MKL_FFT = 1\nUSE_INTEL_LIBM = 1\n$(shell echo "Make.user")" > Make.user
 which icc || exit 1
-grep -i "Make.user" Make.inc
-make test
 # make -j 3
 export PATH=$PAHT:/tmp/julia-source/bin
 julia -e 'versioninfo()'
