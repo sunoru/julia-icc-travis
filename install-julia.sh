84e0c35553b1c6d0e4d60e222a4de5b4f0eb3c02
sunoru <s@sunoru.com>
2016-03-12 15:24:17 +0800

    use root when necessary..

--- install-julia.sh
+++ install-julia.sh
@@ -9,7 +9,7 @@ fi
 wget -q -O /tmp/install-icc.sh \
     "https://raw.githubusercontent.com/sunoru/julia-icc-travis/master/install-icc.sh"
 chmod 755 /tmp/install-icc.sh
-/tmp/install-icc.sh --components icc,ifort,mkl --dest /opt/intel || exit 1
+sudo /tmp/install-icc.sh --components icc,ifort,mkl --dest /opt/intel || exit 1
 source /opt/intel/bin/compilervars.sh intel64 && echo "Source completed"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/intel/ism/bin/intel64
 
