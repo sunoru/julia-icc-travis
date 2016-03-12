773de9ec7769f13bbbe986fffeb09fcd63a7c4d8
sunoru <s@sunoru.com>
2016-03-12 16:22:35 +0800

    ls

--- install-julia.sh
+++ install-julia.sh
@@ -29,7 +29,8 @@ echo "USE_INTEL_MKL = 1" >> Make.user
 echo "USE_INTEL_MKL_FFT = 1" >> Make.user
 echo "USE_INTEL_LIBM = 1" >> Make.user
 which icc || exit 1
-ls /opt/intel/compilers_and_libraries_2016.0.109/linux/compiler/lib/intel64/libimf
+ls -alh /opt/intel/compilers_and_libraries_2016.0.109/linux/compiler/lib/intel64
+ls -alh /opt/intel/compilers_and_libraries_2016.0.109/linux/mkl/lib/intel64
 #make -j 3
 #cat /tmp/julia-source/deps/libuv/config.log
 export PATH=$PAHT:/tmp/julia-source/bin
